04099ac6ca7331df42160446b28115ae1c37042c
PiyushAmarRahe <131381647+PiyushAmarRahe@users.noreply.github.com>
2023-07-14 10:48:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/JoinIndianNavy_007")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/FounderOfIlluminati")
         self.LOGGER(__name__).info(f""" \n\n       
 ▄▀▀▀▄▄▄▄▄▄▄▀▀▀▄
 █▒▒░░░░░░░░░▒▒█
